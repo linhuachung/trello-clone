1669c85d290409ff1f0f2f06b37be53abadaf113
Chun <chunglh@bap.jp>
2023-11-21 17:07:30 +0700

    limit stripe payment

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,7 +2,7 @@
 
 import React, {useEffect, useState} from 'react';
 import {CardModal} from "@/components/modals/card-modal";
-import ProModal from "@/components/modals/pro-modal";
+import {ProModal} from "@/components/modals/pro-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false)
